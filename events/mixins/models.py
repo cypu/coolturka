8040ea3dcbb6c65938c79eb86647b006631867ac
Piotr <szczepanski.peter@gmail.com>
2016-02-25 12:57:16 +0100

    add model places

--- events/mixins/models.py
+++ events/mixins/models.py
@@ -10,8 +10,8 @@ class Event(models.Model):
     buy_ticket_url = models.CharField(u'URL which allows to buy a ticket')
     ticket_prices = models.TextField(u'ticket prices')
     descritpion = models.TextField(u'event descritpion')
-    latitude = models.DecimalField(u'latitude', blank=True, null=True)
-    longitude = models.DecimalField(u'longitude', blank=True, null=True)
+    latitude = models.DecimalField(u'latitude', blank=True, null=True, max_digits=6, decimal_places=4)
+    longitude = models.DecimalField(u'longitude', blank=True, null=True, max_digits=6, decimal_places=4)
     tags = models.CharField(u'tags related to event', max_length=255)
 
     class Meta:
--- places/models.py
+++ places/models.py
@@ -2,4 +2,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+
+    name = models.CharField(max_length=500)
+    #city = models.ForeignKey()
+    latitude = models.DecimalField(u'latitude', blank=True, null=True, max_digits=6, decimal_places=4)
+    longitude = models.DecimalField(u'longitude', blank=True, null=True, max_digits=6, decimal_places=4)
+    home_page = models.URLField()
+    fb_page = models.URLField()
+    open_close_info = models.TextField()
+    description = models.TextField()
+
