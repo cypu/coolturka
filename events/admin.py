658ff6aa9297d4fe9c14d4760e58c24a3602a259
Piotr <szczepanski.peter@gmail.com>
2016-02-29 10:42:16 +0100

    add simple admin panels

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from events.models.concert import Concert
+from events.models.excibition import Excibition
+from events.models.meeting import Metting
+from events.models.play import Play
+from events.models.workshop import Workshop
+
+
+class ConcertAdmin(admin.ModelAdmin):
+    pass
+
+
+class ExcibitionAdmin(admin.ModelAdmin):
+    pass
+
+
+class MettingAdmin(admin.ModelAdmin):
+    pass
+
+
+class PlayAdmin(admin.ModelAdmin):
+    pass
+
+
+class WorkshopAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.register.site(Concert, ConcertAdmin)
+admin.register.site(Excibition, ExcibitionAdmin)
+admin.register.site(Metting, MettingAdmin)
+admin.register.site(Play, PlayAdmin)
+admin.register.site(Workshop, WorkshopAdmin)
+
--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from locations.models.city import City
+from locations.models.voivodeship import Voivodeship
 
-# Register your models here.
+
+class CityAdmin(admin.ModelAdmin):
+    pass
+
+
+class VoivodeshipAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(City, CityAdmin)
+admin.site.register(Voivodeship, VoivodeshipAdmin)
